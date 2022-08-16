f5db0f0612aa57afb146f4d083e0718010201cdd
Maksym Skalozub <94378273+maksym-skalozub@users.noreply.github.com>
2022-08-16 11:28:48 -0400

    Update script.js
    update howemork

--- homework3/script.js
+++ homework3/script.js
@@ -1,3 +1,5 @@
+//task 1////==========================================//
+console.log("//task 1////==========================================//")
 const usersArray = [
     {
       name: "John",
@@ -52,23 +54,76 @@ const usersArray = [
         },
    ];
 
-//task 1////==========================================//
-console.log("//task 1////==========================================//")
 
-let richestUser = {name: "", balance: 0};
+let richestUser = "";
+let balance = 0;
 for (let i = 0; i < usersArray.length; i++){
-    if (usersArray[i].balance.slice(0, -1) > richestUser.balance ){
-        richestUser.name = usersArray[i].name;
-        richestUser.balance = Number(usersArray[i].balance.slice(0, -1));
+    if (usersArray[i].balance.slice(0, -1) > balance ){
+        richestUser = usersArray[i].name;
+        balance = Number(usersArray[i].balance.slice(0, -1));
     }
 }
 
-console.log(richestUser.name)
+console.log(richestUser)
 
 //task 2////==========================================//
 console.log("//task 2////==========================================//")
+const usersArray2 = [
+    {
+      name: "John",
+      balance: '2000$',
+      friends: [
+        {
+        name: "Robert",
+        balance: '1000$',
+        },
+        {
+        name: "Kate",
+        balance: '0$',
+        },
+        ],
+        },
+    {
+      name: "Isaac",
+      balance: '20000$',
+      friends: [
+        {
+        name: "Bob",
+        balance: '2000$',
+        },
+        {
+        name: "Robert",
+        balance: '150000$',
+        },
+        ],
+        },
+    {
+      name: "Jane",
+      balance: '200$',
+      friends: [
+        {
+        name: "Kate",
+        balance: '5000$',
+        },
+        {
+        name: "Anne",
+        balance: '1234$',
+        },
+        {
+        name: "Bob",
+        balance: '300$',
+        },
+        ],
+        },
+    {
+      name: "Russell",
+      balance: '100$',
+      friends: [],
+        },
+   ];
 
-usersArray.sort(function (a, b) {
+
+usersArray2.sort(function (a, b) {
     if (a.name < b.name) {
       return -1;
     }
@@ -78,87 +133,535 @@ usersArray.sort(function (a, b) {
     return 0;
   });
 
-console.log(usersArray);
+console.log(usersArray2);
 
 //task 3////==========================================//
 console.log("//task 3////==========================================//")
+const usersArray3 = [
+    {
+      name: "John",
+      balance: '2000$',
+      friends: [
+        {
+        name: "Robert",
+        balance: '1000$',
+        },
+        {
+        name: "Kate",
+        balance: '0$',
+        },
+        ],
+        },
+    {
+      name: "Isaac",
+      balance: '20000$',
+      friends: [
+        {
+        name: "Bob",
+        balance: '2000$',
+        },
+        {
+        name: "Robert",
+        balance: '150000$',
+        },
+        ],
+        },
+    {
+      name: "Jane",
+      balance: '200$',
+      friends: [
+        {
+        name: "Kate",
+        balance: '5000$',
+        },
+        {
+        name: "Anne",
+        balance: '1234$',
+        },
+        {
+        name: "Bob",
+        balance: '300$',
+        },
+        ],
+        },
+    {
+      name: "Russell",
+      balance: '100$',
+      friends: [],
+        },
+   ];
+
 
 let summOfUsers = 0;
-for (let i = 0; i < usersArray.length; i++){
-    summOfUsers+= Number(usersArray[i].balance.slice(0, -1));
+for (let i = 0; i < usersArray3.length; i++){
+    summOfUsers+= Number(usersArray3[i].balance.slice(0, -1));
 }
 
 console.log(summOfUsers);
 
 //task 4////==========================================//
 console.log("//task 4////==========================================//")
+const usersArray4 = [
+    {
+      name: "John",
+      balance: '2000$',
+      friends: [
+        {
+        name: "Robert",
+        balance: '1000$',
+        },
+        {
+        name: "Kate",
+        balance: '0$',
+        },
+        ],
+        },
+    {
+      name: "Isaac",
+      balance: '20000$',
+      friends: [
+        {
+        name: "Bob",
+        balance: '2000$',
+        },
+        {
+        name: "Robert",
+        balance: '150000$',
+        },
+        ],
+        },
+    {
+      name: "Jane",
+      balance: '200$',
+      friends: [
+        {
+        name: "Kate",
+        balance: '5000$',
+        },
+        {
+        name: "Anne",
+        balance: '1234$',
+        },
+        {
+        name: "Bob",
+        balance: '300$',
+        },
+        ],
+        },
+    {
+      name: "Russell",
+      balance: '100$',
+      friends: [],
+        },
+   ];
 
-for (let i = 0; i < usersArray.length; i++){
-    if (usersArray[i].name == "Russell") {
-        usersArray.splice(i, 1);
+
+for (let i = 0; i < usersArray4.length; i++){
+    if (usersArray4[i].name === "Russell") {
+        usersArray4.splice(i, 1);
     }
 }
 
-console.log(usersArray)
+console.log(usersArray4)
 
 //task 5////==========================================//
 console.log("//task 5////==========================================//")
+const usersArray5 = [
+    {
+      name: "John",
+      balance: '2000$',
+      friends: [
+        {
+        name: "Robert",
+        balance: '1000$',
+        },
+        {
+        name: "Kate",
+        balance: '0$',
+        },
+        ],
+        },
+    {
+      name: "Isaac",
+      balance: '20000$',
+      friends: [
+        {
+        name: "Bob",
+        balance: '2000$',
+        },
+        {
+        name: "Robert",
+        balance: '150000$',
+        },
+        ],
+        },
+    {
+      name: "Jane",
+      balance: '200$',
+      friends: [
+        {
+        name: "Kate",
+        balance: '5000$',
+        },
+        {
+        name: "Anne",
+        balance: '1234$',
+        },
+        {
+        name: "Bob",
+        balance: '300$',
+        },
+        ],
+        },
+    {
+      name: "Russell",
+      balance: '100$',
+      friends: [],
+        },
+   ];
 
-for (let i = 0; i < usersArray.length; i++){
-    if (usersArray[i].name == "Isaac") {
-        usersArray.splice(i, 1);
+
+for (let i = 0; i < usersArray5.length; i++){
+    if (usersArray5[i].name === "Isaac") {
+        usersArray5.splice(i, 1);
     }
 }
 
-console.log(usersArray)
+console.log(usersArray5)
 
 //task 6////==========================================//
 console.log("//task 6////==========================================//")
+const usersArray6 = [
+    {
+      name: "John",
+      balance: '2000$',
+      friends: [
+        {
+        name: "Robert",
+        balance: '1000$',
+        },
+        {
+        name: "Kate",
+        balance: '0$',
+        },
+        ],
+        },
+    {
+      name: "Isaac",
+      balance: '20000$',
+      friends: [
+        {
+        name: "Bob",
+        balance: '2000$',
+        },
+        {
+        name: "Robert",
+        balance: '150000$',
+        },
+        ],
+        },
+    {
+      name: "Jane",
+      balance: '200$',
+      friends: [
+        {
+        name: "Kate",
+        balance: '5000$',
+        },
+        {
+        name: "Anne",
+        balance: '1234$',
+        },
+        {
+        name: "Bob",
+        balance: '300$',
+        },
+        ],
+        },
+    {
+      name: "Russell",
+      balance: '100$',
+      friends: [],
+        },
+   ];
+
 
 let userHarry = {name: "Harry", balance: "500$"};
-for (let i = 0; i < usersArray.length; i++){
-    if (usersArray[i].name == "John") {
-        usersArray.splice(i+1, 0, userHarry);
+for (let i = 0; i < usersArray6.length; i++){
+    if (usersArray6[i].name === "John") {
+        usersArray6.splice(i+1, 0, userHarry);
         break;
     }
 }
 
-console.log(usersArray)
+console.log(usersArray6)
 
 //task 7////==========================================//
 console.log("//task 7////==========================================//")
+const usersArray7 = [
+    {
+      name: "John",
+      balance: '2000$',
+      friends: [
+        {
+        name: "Robert",
+        balance: '1000$',
+        },
+        {
+        name: "Kate",
+        balance: '0$',
+        },
+        ],
+        },
+    {
+      name: "Isaac",
+      balance: '20000$',
+      friends: [
+        {
+        name: "Bob",
+        balance: '2000$',
+        },
+        {
+        name: "Robert",
+        balance: '150000$',
+        },
+        ],
+        },
+    {
+      name: "Jane",
+      balance: '200$',
+      friends: [
+        {
+        name: "Kate",
+        balance: '5000$',
+        },
+        {
+        name: "Anne",
+        balance: '1234$',
+        },
+        {
+        name: "Bob",
+        balance: '300$',
+        },
+        ],
+        },
+    {
+      name: "Russell",
+      balance: '100$',
+      friends: [],
+        },
+   ];
+
 
 let userConor = {name: "Conor", balance: "2500$"};
-usersArray.push(userConor);
+usersArray7.push(userConor);
 
-console.log(usersArray)
+console.log(usersArray7)
 
 //task 8////==========================================//
 console.log("//task 8////==========================================//")
+const usersArray8 = [
+    {
+      name: "John",
+      balance: '2000$',
+      friends: [
+        {
+        name: "Robert",
+        balance: '1000$',
+        },
+        {
+        name: "Kate",
+        balance: '0$',
+        },
+        ],
+        },
+    {
+      name: "Isaac",
+      balance: '20000$',
+      friends: [
+        {
+        name: "Bob",
+        balance: '2000$',
+        },
+        {
+        name: "Robert",
+        balance: '150000$',
+        },
+        ],
+        },
+    {
+      name: "Jane",
+      balance: '200$',
+      friends: [
+        {
+        name: "Kate",
+        balance: '5000$',
+        },
+        {
+        name: "Anne",
+        balance: '1234$',
+        },
+        {
+        name: "Bob",
+        balance: '300$',
+        },
+        ],
+        },
+    {
+      name: "Russell",
+      balance: '100$',
+      friends: [],
+        },
+   ];
 
-function myFunctionOutputUsersAndFriends(users) {
+const usersAndFriendsArray = [];
+function myFunctionOutputUsersAndFriends(users, usersAndFriends) {
     for (let i = 0; i < users.length; i++){
-        console.log(users[i])
+        usersAndFriends.push(users[i]);
         if (users[i].friends){
-            myFunctionOutputUsersAndFriends(users[i].friends);
+            myFunctionOutputUsersAndFriends(users[i].friends, usersAndFriends);
         }
     }
+    return usersAndFriends;
 }
 
-myFunctionOutputUsersAndFriends(usersArray);
+myFunctionOutputUsersAndFriends(usersArray8, usersAndFriendsArray);
+
+const uniqueUsersAndFriends = []
+const unique = usersAndFriendsArray.filter(element => {
+    const isDuplicate = uniqueUsersAndFriends.includes(element.name);
+  
+    if (!isDuplicate) {
+      uniqueUsersAndFriends.push(element.name);
+  
+      return true;
+    }
+  
+    return false;
+  });
+
+console.log(unique);
 
 //task 9////==========================================//
 console.log("//task 9////==========================================//")
+const usersArray9 = [
+    {
+      name: "John",
+      balance: '2000$',
+      friends: [
+        {
+        name: "Robert",
+        balance: '1000$',
+        },
+        {
+        name: "Kate",
+        balance: '0$',
+        },
+        ],
+        },
+    {
+      name: "Isaac",
+      balance: '20000$',
+      friends: [
+        {
+        name: "Bob",
+        balance: '2000$',
+        },
+        {
+        name: "Robert",
+        balance: '150000$',
+        },
+        ],
+        },
+    {
+      name: "Jane",
+      balance: '200$',
+      friends: [
+        {
+        name: "Kate",
+        balance: '5000$',
+        },
+        {
+        name: "Anne",
+        balance: '1234$',
+        },
+        {
+        name: "Bob",
+        balance: '300$',
+        },
+        ],
+        },
+    {
+      name: "Russell",
+      balance: '100$',
+      friends: [],
+        },
+   ];
 
-for (let i = 0; i < usersArray.length; i++){
-    if (Number(usersArray[i].balance.slice(0, -1)) > 2000 ){
-        console.log(usersArray[i])
+
+for (let i = 0; i < usersArray9.length; i++){
+    if (Number(usersArray9[i].balance.slice(0, -1)) > 2000 ){
+        console.log(usersArray9[i])
     }
 }
 
 
 //task 10////==========================================//
 console.log("//task 10////==========================================//")
+const usersArray10 = [
+    {
+      name: "John",
+      balance: '2000$',
+      friends: [
+        {
+        name: "Robert",
+        balance: '1000$',
+        },
+        {
+        name: "Kate",
+        balance: '0$',
+        },
+        ],
+        },
+    {
+      name: "Isaac",
+      balance: '20000$',
+      friends: [
+        {
+        name: "Bob",
+        balance: '2000$',
+        },
+        {
+        name: "Robert",
+        balance: '150000$',
+        },
+        ],
+        },
+    {
+      name: "Jane",
+      balance: '200$',
+      friends: [
+        {
+        name: "Kate",
+        balance: '5000$',
+        },
+        {
+        name: "Anne",
+        balance: '1234$',
+        },
+        {
+        name: "Bob",
+        balance: '300$',
+        },
+        ],
+        },
+    {
+      name: "Russell",
+      balance: '100$',
+      friends: [],
+        },
+   ];
+
 
 let richestUserIncludeFriends = {name: "", balance: 0};
 function myFunctionRichestUser(users, richest) {
@@ -174,34 +677,87 @@ function myFunctionRichestUser(users, richest) {
     return richest
 }
 
-myFunctionRichestUser(usersArray, richestUserIncludeFriends);
+myFunctionRichestUser(usersArray10, richestUserIncludeFriends);
 
-console.log(richestUserIncludeFriends.name)
+console.log(richestUserIncludeFriends.name);
 
 
 //task 11////==========================================//
 console.log("//task 11////==========================================//")
+const usersArray11 = [
+    {
+      name: "John",
+      balance: '2000$',
+      friends: [
+        {
+        name: "Robert",
+        balance: '1000$',
+        },
+        {
+        name: "Kate",
+        balance: '0$',
+        },
+        ],
+        },
+    {
+      name: "Isaac",
+      balance: '20000$',
+      friends: [
+        {
+        name: "Bob",
+        balance: '2000$',
+        },
+        {
+        name: "Robert",
+        balance: '150000$',
+        },
+        ],
+        },
+    {
+      name: "Jane",
+      balance: '200$',
+      friends: [
+        {
+        name: "Kate",
+        balance: '5000$',
+        },
+        {
+        name: "Anne",
+        balance: '1234$',
+        },
+        {
+        name: "Bob",
+        balance: '300$',
+        },
+        ],
+        },
+    {
+      name: "Russell",
+      balance: '100$',
+      friends: [],
+        },
+   ];
 
-let usersWithFriends = []
-for (let i = 0; i < usersArray.length; i++){
-    if (usersArray[i].friends){
-        usersWithFriends.push(usersArray[i]);
+
+const usersFriends = []
+for (let i = 0; i < usersArray11.length; i++){
+    usersFriends.push(usersArray[i].friends);
+}
+console.log(usersFriends)
+const usersFriendNames = []
+for (let i = 0; i < usersFriends.length; i++){
+    usersFriendNames.push([])
+    for (let j = 0; j <usersFriends[i].length; j++){
+        usersFriendNames[i].push(usersFriends[i][j].name)
     }
 }
-
-for (let i = 0; i < usersWithFriends.length; i++){
-    for (let j = 0; j < usersWithFriends[i].friends.length; j++){
-        usersWithFriends[i].friends[j] = usersWithFriends[i].friends[j].name
-    }
-}
-console.log(usersWithFriends)
-for (let i = 0; i < usersWithFriends.length; i++){
-    for (let j = i+1; j < usersWithFriends.length; j++){
-        const contains = usersWithFriends[i].friends.some(element => {
-            return usersWithFriends[j].friends.includes(element);
-        })
-        if (contains) {
-            console.log(`${usersWithFriends[i].name} and ${usersWithFriends[j].name} have same friend`)
+console.log(usersFriendNames)
+for (let i = 0; i < usersFriendNames.length; i++){
+    for (let j = i+1; j < usersFriendNames.length; j++){
+        const intersection = usersFriendNames[i].filter(element => usersFriendNames[j].includes(element));
+        console.log(intersection)
+        if (intersection != "") {
+            console.log(`${usersArray11[i].name} and ${usersArray11[j].name} have mutual friend ${intersection}`)
         }
     }
 }
@@ -209,13 +765,24 @@ for (let i = 0; i < usersWithFriends.length; i++){
 //task 12////==========================================//
 console.log("//task 12////==========================================//")
 
-const arr1 = [10, 'a', '5', 5, 1]; 
+const arr1 = [10, 'a', "5", 5, 1]; 
 const arr2 = [10, 'a', 5, 5, 1];
 
-console.log(arr1 ===arr2);
+let result = true
+for (let i = 0; i < arr1.length; i++){
+    if (arr1[i] !== arr2[i]){
+        result = false;
+        break;
+    }
+}
+if (result) {
+    console.log(`${arr1} equal ${arr2}`)
+} else {
+    console.log(`${arr1} not equal ${arr2}`)
+}
 
-//task 12////==========================================//
-console.log("//task 12////==========================================//")
+//task 13////==========================================//
+console.log("//task 13////==========================================//")
 
 function palindrome(str) {
     const re = /[^A-Za-z0-9]/g;
